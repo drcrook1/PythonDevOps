be297798f849db3d44e2b298857691f21d2876bf
David Crook <drcrook@outlook.com>
2019-01-04 10:41:41 -0500

    Update runtestsbuild.sh

--- Module-Test/runtestsbuild.sh
+++ Module-Test/runtestsbuild.sh
@@ -2,7 +2,7 @@ mkdir $(System.DefaultWorkingDirectory)/junit
 cd ./Module
 docker build -t pydevopsmodule .
 cd ..
-cd ./Module-Tests
+cd ./Module-Test
 docker build -t pydevopsmoduletests .
 cd $(System.DefaultWorkingDirectory)
 docker run -v  $(System.DefaultWorkingDirectory)/junit:/tests/junit pydevopsmoduletests
