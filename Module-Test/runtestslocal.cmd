465c29479907b846d602e983280d3a1b3ff5067b
Bala <bala_b@hotmail.com>
2019-01-30 20:56:45 +0000

    Update runtestslocal.cmd

--- Module-Test/runtestslocal.cmd
+++ Module-Test/runtestslocal.cmd
@@ -2,8 +2,10 @@ docker stop pydevopsmoduletests
 docker rm pydevopsmoduletests
 cd ..
 cd ./Module
+PWD
 docker build -t pydevopsmodule .
 cd ..
 cd ./Module-Test
+PWD
 docker build -t pydevopsmoduletests .
 docker run pydevopsmoduletests
